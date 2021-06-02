92c81da43cc47416ce2acfa18b018013451ab016
Alfredo Razo Garduño <32911082+AlfredoRazo@users.noreply.github.com>
2021-06-02 00:10:45 -0500

    campos nuevos
    se agregan campos nuevos operaciones

--- src/app/pages/operaciones/operaciones-nuevo-pago/operaciones-nuevo-pago.component.ts
+++ src/app/pages/operaciones/operaciones-nuevo-pago/operaciones-nuevo-pago.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./operaciones-nuevo-pago.component.css']
 })
 export class OperacionesNuevoPagoComponent implements OnInit {
+  tipoArribo = [{id: 1, descripcion : 'Regular'},{id: 2, descripcion : 'Forzoso'},{id: 3, descripcion : 'Combustible'},{id: 4, descripcion : 'Imprevisto'},{id: 5, descripcion : 'Reparación'}];
+  tipoTrafico = [{id: 1, descripcion : 'Alta'},{id: 2, descripcion : 'Cabotaje'}];
+  tipoActividad = [{id: 1, descripcion : 'Comercial'},{id: 2, descripcion : 'Pesquera'},{id: 3, descripcion : 'Riberño'}];
+  tipoArancel = [{id: 1, descripcion : 'Atraque'},{id: 2, descripcion : 'Puerto Fijo'},{id: 1, descripcion : 'Puerto Variable'}];
   pageAtraque = 1;
   dataAtraque = [
     {
