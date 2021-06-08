0a785f82bb721d3df0cb9a338428e3ade27848b3
Alfredo Razo Garduño <32911082+AlfredoRazo@users.noreply.github.com>
2021-06-08 09:25:11 -0500

    form post

--- src/app/pages/facturacion/facturacion-nueva-consulta/facturacion-nueva-consulta.component.ts
+++ src/app/pages/facturacion/facturacion-nueva-consulta/facturacion-nueva-consulta.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from '@env/environment';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-facturacion-nueva-consulta',
@@ -6,14 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./facturacion-nueva-consulta.component.css']
 })
 export class FacturacionNuevaConsultaComponent implements OnInit {
+  errorMsj = '';
+  hasError = false;
+  hasSuccess = false;
+  successMsj = '';
 
-  constructor() { }
+  constructor(
+    private spinner: NgxSpinnerService,
+    public http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
   submit(form : any): void{
-    console.log(form.value);
+    this.spinner.show();
+    this.http.post<any>(environment.endpoint + 'consultasap', form.value).subscribe(res => {
+      this.spinner.hide();
+      console.log(res);
+      if(res[0].error){
+        this.hasSuccess = false;
+        this.successMsj = ''
+        this.hasError = true;
+        this.errorMsj = res[0].errorStr;
+      }else{
+        form.reset();
+        form.resetForm();
+        this.hasError = false;
+        this.errorMsj = '';
+        this.hasSuccess = true;
+        this.successMsj = 'Se guardó correctamente, número de consulta : ' + res[0].noConsulta;
+      }
+    }, error => {
+      this.spinner.hide();
+    })
 
   }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  endpoint: 'http://zzone.cloudapp.net/sicrefis/api/',
+  endpoint: ' https://web.zonazero.cloud/sicrefis/api/',
   endpointApi: 'https://pis-apiman-restfinav1.azurewebsites.net/API/',
   appKey: 'c53ea43376d653a43e10711de2da2d9b6f156ead',
   googleCaptchaKey: '6LcZ7w4bAAAAAAcogck-Te91o6SPDZYuNcWBlmHX'
