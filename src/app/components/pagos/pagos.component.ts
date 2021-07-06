344b10e1b7845a8fd3a3c6272718c56b8261e05b
Alfredo RG <razogarduno@gmail.com>
2021-07-05 19:32:31 -0500

    pagos

--- src/app/components/pagos/pagos.component.ts
+++ src/app/components/pagos/pagos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '@env/environment';
 
 @Component({
   selector: 'app-pagos',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagosComponent implements OnInit {
 
+  monto = '';
+  banco = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  sendPago() : void {
+    if(this.banco == 'santander'){
+    const multiPagosform = document.createElement('form');
+    const convenio = document.createElement('input');
+    const referencia = document.createElement('input');
+    const importe = document.createElement('input');
+    const url_resp = document.createElement('input');
+    
+    multiPagosform.method = 'POST';
+    multiPagosform.action = environment.santanderEndpoint;
+    convenio.value = '10000';
+    convenio.name = 'convenio';
+    multiPagosform.appendChild(convenio);
+    referencia.value = '1000000'
+    referencia.name = 'referencia';
+    multiPagosform.appendChild(referencia);
+    importe.value = this.monto;
+    importe.name = 'importe';
+    multiPagosform.appendChild(importe);
+    importe.value = this.monto;
+    importe.name = 'importe';
+    multiPagosform.appendChild(importe);
+    url_resp.value = 'http://pismzo.azurewebsites.net/pis/';
+    url_resp.name = 'url_resp';
+    multiPagosform.appendChild(url_resp);
+    
+    document.body.appendChild(multiPagosform);
+    multiPagosform.submit();
+    }
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,7 @@ export const environment = {
   endpointCat: 'https://pis-catalogos.azurewebsites.net/api/',
   appKey: 'c53ea43376d653a43e10711de2da2d9b6f156ead',
   googleCaptchaKey: '6LcZ7w4bAAAAAAcogck-Te91o6SPDZYuNcWBlmHX',
+  santanderEndpoint: 'https://www.santander.com.mx/Supernet2007/homeMicrositio.jsp',
   catlogin:{
     usuario: "zz",
     password: "API-MAN-PIS-20!0M3d1n$4!",
