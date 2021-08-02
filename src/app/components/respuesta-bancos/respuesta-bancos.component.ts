007478cd3da61578d3cfe08a23717ef7313cfd12
Alfredo RG <razogarduno@gmail.com>
2021-08-01 23:33:44 -0500

    paginado pagos

--- src/app/components/respuesta-bancos/respuesta-bancos.component.ts
+++ src/app/components/respuesta-bancos/respuesta-bancos.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { environment } from '@env/environment';
+import { HelpersService } from '@serv/helpers.service';
+import { PaginateService } from '@serv/paginate.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 declare var $: any;
 @Component({
@@ -12,14 +14,21 @@ export class RespuestaBancosComponent implements OnInit {
 
   constructor(
     private http: HttpClient,
-    private spinner: NgxSpinnerService
+    private pagina: PaginateService,
+    private spinner: NgxSpinnerService,
+    private help: HelpersService
   ) { }
 
   data: any[] = [];
-  fechaini: any;
-  fechafin: any;
-  
+  originalData: any[] = [];
+  total = 0;
+  page = 1;
+  collSize = 10;
+  fechaini = this.help.today();
+  fechafin = this.help.today();
+  descPaginado = '';
   ngOnInit(): void {
+    this.getData();
     $('#fecha-inis').datepicker({ dateFormat: 'dd-mm-yy', onSelect: (date: any) => { this.fechaini = date } });
     $('#fecha-fins').datepicker({ dateFormat: 'dd-mm-yy', onSelect: (date: any) => { this.fechafin = date } });
   }
@@ -28,9 +37,24 @@ export class RespuestaBancosComponent implements OnInit {
     this.spinner.show();
     this.http.get(`${environment.endpointApi}bancosRespuesta?fechainicial=${this.fechaini}&fechafinal=${this.fechafin}`)
       .subscribe((res: any) => {
-        this.data = res[0];
+       if(Array.isArray(res[0])){
+        this.total = res[0].length;
+        this.originalData = [...res[0]];
+        this.data = this.pagina.paginate(res[0], this.collSize, this.page);
+       }
         this.spinner.hide();
       }, (error) => {this.spinner.hide(); });
+  }
+  paginado(evt: any = null): void {
+
+      this.data = this.pagina.paginate(this.originalData, 10, this.page);
+    this.descripcionPaginado();
+  }
+  descripcionPaginado(): void {
+    var numberOfPages = Math.floor((this.total + this.collSize - 1) / this.collSize);
+    var start = (this.page * this.collSize) - (this.collSize - 1);
+    var end = Math.min(start + this.collSize - 1, this.total);
+    this.descPaginado = `Registros del <b>${start}</b> al <b>${end}</b>`;
   }
 
 }
