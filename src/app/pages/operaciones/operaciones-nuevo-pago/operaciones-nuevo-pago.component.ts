081175a7c9b096b2203d0db18cb8b17dd81e773a
Alfredo RG <razogarduno@gmail.com>
2021-08-03 19:10:20 -0500

    operaciones

--- src/app/pages/operaciones/operaciones-nuevo-pago/operaciones-nuevo-pago.component.ts
+++ src/app/pages/operaciones/operaciones-nuevo-pago/operaciones-nuevo-pago.component.ts
@@ -68,7 +68,12 @@ export class OperacionesNuevoPagoComponent implements OnInit {
     '12',
     '13',
     '14',
-    '15'
+    '15',
+    '16',
+    '17',
+    '18',
+    '19',
+    '20'
   ]
   search: any = (text$: Observable<any>) =>
     text$.pipe(
