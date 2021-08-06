0fde0fb4aace3b00aff4860aaa25f5795618c930
Alfredo RG <razogarduno@gmail.com>
2021-08-06 16:12:06 -0500

    bbva

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -11,8 +11,8 @@ export const environment = {
   bbvaEndpoint: 'https://prepro.adquiracloud.mx/clb/endpoint/apimanzanillo',
   //bbvaEndpoint: 'https://www.egbs1.com.mx/clb/endpoint/apimanzanillo',
   bbvaKey: '85K98s7653Ap383iMangt73iO393K0jNjs031Man93k03lo39zAks3llo33fJU3092er93ai3Lj3',
-  bbvaSuccessRes: 'https://pis-app-finanzas-qa.azurewebsites.net/finanzas-api/bbva10',
-  bbvaFailRes: 'https://pis-app-finanzas-qa.azurewebsites.net/finanzas-api/santanderE10',
+  bbvaSuccessRes: 'https://pis-apiman-restfinav1.azurewebsites.net/api/bbvav1QA',
+  bbvaFailRes: 'https://pis-apiman-restfinav1.azurewebsites.net/api/bbvav1QA',
   catlogin:{
     usuario: "zz",
     password: "API-MAN-PIS-20!0M3d1n$4!",
