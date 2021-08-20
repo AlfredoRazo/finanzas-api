eafef3a7568b7fcea7fffb1c2f95ba495f569d42
Alfredo RG <razogarduno@gmail.com>
2021-08-19 23:12:11 -0500

    manifiesto

--- src/app/pages/recinto/recinto.component.ts
+++ src/app/pages/recinto/recinto.component.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { environment } from '@env/environment';
 import { AuthService } from '@serv/auth.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 declare var $: any;
 
 @Component({
@@ -10,7 +11,8 @@ declare var $: any;
   styleUrls: ['./recinto.component.css']
 })
 export class RecintoComponent implements OnInit {
-  
+  manifiesto = '';
+  manifiestoData:any;
   submenu = 1;
   page = 1;
   data = [
@@ -24,8 +26,19 @@ export class RecintoComponent implements OnInit {
   ];
 
   constructor(private auth: AuthService,
+    private spinner: NgxSpinnerService,
     private http: HttpClient) { }
     ngOnInit(): void {
     
+    }
+
+    consultarManifiesto(): void{
+      this.spinner.show();
+      this.http.get(`${environment.endpointRecinto}manifiesto/v1/num/${this.manifiesto}`).subscribe((res: any) =>{
+        this.manifiestoData = res.datos;
+        this.spinner.hide();
+      },err=>{
+        this.spinner.hide();
+      });
     }
 }
